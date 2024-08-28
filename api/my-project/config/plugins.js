7d12959ecfeac56c1fb968c37aa27384c897343a
YourNewUsername <yournewemail@example.com>
2024-08-28 23:17:02 +0530

    changes

--- api/my-project/config/plugins.js
+++ api/my-project/config/plugins.js
@@ -1,20 +1,20 @@
 module.exports = ({ env }) => ({
     email: {
       config: {
-        provider: 'strapi-provider-email-nodemailer',
+        provider: 'strapi-provider-email-smtp',
         providerOptions: {
-          host: env('SMTP_HOST', 'smtp.gmail.com'),
-          port: env.int('SMTP_PORT', 465),
+          host: env('SMTP_HOST', process.env.SMTP_HOST), //smtp.gmail.com
+          port: env.int('SMTP_PORT', process.env.SMTP_PORT),// 465
           auth: {
-            user: env('SMTP_USER', 'shaileshdarji1321@gmail.com'),
-            pass: env('SMTP_PASS', 'tqji wgbp gtwm yats'),
+            user: env('SMTP_USER', process.env.SMTP_USER), // shaileshdarji1321@gmail.com //noreply@plush.fun
+            pass: env('SMTP_PASS', process.env.SMTP_PASS), // tqji wgbp gtwm yats //  vraq-ofjf-rvmf-qjil
           },
           // Optional settings
           secure: true,
         },
         settings: {
-          defaultFrom: env('DEFAULT_FROM', 'shaileshdarji1321@gmail.com'),
-          defaultReplyTo: env('DEFAULT_REPLY_TO', 'shaileshdarji1321@gmail.com'),
+          defaultFrom: env('DEFAULT_FROM', process.env.SMTP_USER),
+          defaultReplyTo: env('DEFAULT_REPLY_TO', process.env.SMTP_USER),
         },
       },
     },
